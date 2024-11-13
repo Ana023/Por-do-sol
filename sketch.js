dccc6238db6b0d0e92db913a085a33d6c53baa6f
Ana023 <ana.silveira.almeida@escola.pr.gov.br>
2024-11-13 09:08:21 -0300

    13

---
+++ sketch.js
@@ -0,0 +1,27 @@
+< l l
+  ang="en"
+  <he<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.1/p5.js"></script>
+    <script src=></script>
+    <link rel="stylesheet" type="te< l l
+  ang="en"
+  <he<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.1/p5.js"></script>
+    <script src=></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+
+  </head>
+  <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
+  </body>
+</l>xt/css" href="style.css">
+    <meta charset="utf-8" />
+
+  </head>
+  <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
+  </body>
+</l>
